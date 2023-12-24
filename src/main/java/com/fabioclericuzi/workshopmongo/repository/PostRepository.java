ac74b523b68dd127180659f0fde5bcba52b87ea9
unknown <fabioclericuzilima@hotmail.com>
2023-12-24 12:09:29 -0300

    Criaçãoo de método para retornar posts pelo title

--- src/main/java/com/fabioclericuzi/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fabioclericuzi/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.fabioclericuzi.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fabioclericuzi.workshopmongo.domain.Post;
@@ -10,8 +11,10 @@ import com.fabioclericuzi.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	List<Post> findBytitleContainingIgnoreCase(String text);
+	@Query("{ 'title': { $regex: ?0, $options: i } }")
+	List<Post> findByTitle(String text);
 	
+	List<Post> findBytitleContainingIgnoreCase(String text);
 	
 	
 }
